0606bd69567d273f79e416e37827e3de810a3653
gods1son <tolulopeibiyode@gmail.com>
2017-03-23 23:43:01 -0400

    all fixed

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ function setAlarm(){
     var d = new Date();
     d.setHours(hour);
     d.setMinutes(minute);
+    d.setSeconds(0);
 
 cordova.plugins.notification.local.schedule({
-    text: "Delayed Notification",
+    text: "Notification Test",
     at: d,
-    led: "FF0000",
-    sound: null
+    led: "blue",
+    sound: null,
+},function callback(){
+    hour.value = '0';
+    minute.value = '0';
+    alert("alarm set");
 });
 }
