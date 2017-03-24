dc0601636a0e9a3ae4d2c0caf225e733a816d79f
gods1son <tolulopeibiyode@gmail.com>
2017-03-23 23:52:26 -0400

    fixed bug

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ function setAlarm(){
     hour = parseInt(hour.value);
     var minute = document.getElementById("setminute");
     minute = parseInt(minute.value);
+    var reason = document.getElementById("setreason");
     var d = new Date();
     d.setHours(hour);
     d.setMinutes(minute);
     d.setSeconds(0);
 
 cordova.plugins.notification.local.schedule({
-    text: "Notification Test",
+    text: reason.value,
     at: d,
     led: "blue",
     sound: null,
-},function callback(){
-    hour.value = '0';
-    minute.value = '0';
-    alert("alarm set");
 });
+    alert("reminder set for " + reason.value);
+    document.getElementById("sethour").value = "0";
+    document.getElementById("setminute").value = "0";
+    document.getElementById("setreason").value = "";
+
 }
